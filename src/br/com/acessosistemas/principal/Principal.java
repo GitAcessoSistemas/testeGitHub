81d55f81429ca2173e81d2bf1b42c0fffcace85e
Acesso Sistemas <92989901+GitAcessoSistemas@users.noreply.github.com>
2021-10-22 11:46:05 -0300

    Add files via upload

---
+++ src/br/com/acessosistemas/principal/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.acessosistemas.principal;
+
+/**
+ * @author Hugo
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("teste hugo");
+
+	}
+
+}
